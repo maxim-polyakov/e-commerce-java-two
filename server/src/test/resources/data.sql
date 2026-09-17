de1e4e9ba8d1f37dcea2bac9c43992b04ed6e298
maxim-polyakov <maxim7012@gmail.com>
2026-09-18 02:30:03 +0300

    Update data.sql

--- server/src/test/resources/data.sql
+++ server/src/test/resources/data.sql
@@ -11,19 +11,19 @@ INSERT INTO address(address_line, city, country, user_id)
     VALUES ('123 Tester Hill', 'Testerton', 'England', 1)
     , ('312 Spring Boot', 'Hibernate', 'England', 3);
 
-INSERT INTO product (name, short_description, long_description, price, raiting, image)
-    VALUES ('Product #1', 'Product one short description.', 'This is a very long description of product #1.', 5.50, 0.0, '/images/test-product-1.png')
-    , ('Product #2', 'Product two short description.', 'This is a very long description of product #2.', 10.56, 0.0, '/images/test-product-2.png')
-    , ('Product #3', 'Product three short description.', 'This is a very long description of product #3.', 2.74, 0.0, '/images/test-product-3.png')
-    , ('Product #4', 'Product four short description.', 'This is a very long description of product #4.', 15.69, 0.0, '/images/test-product-4.png')
-    , ('Product #5', 'Product five short description.', 'This is a very long description of product #5.', 42.59, 0.0, '/images/test-product-5.png');
+INSERT INTO product (name, short_description, long_description, price, raiting, image, deleted)
+    VALUES ('Product #1', 'Product one short description.', 'This is a very long description of product #1.', 5.50, 0.0, '/images/test-product-1.png', false)
+    , ('Product #2', 'Product two short description.', 'This is a very long description of product #2.', 10.56, 0.0, '/images/test-product-2.png', false)
+    , ('Product #3', 'Product three short description.', 'This is a very long description of product #3.', 2.74, 0.0, '/images/test-product-3.png', false)
+    , ('Product #4', 'Product four short description.', 'This is a very long description of product #4.', 15.69, 0.0, '/images/test-product-4.png', false)
+    , ('Product #5', 'Product five short description.', 'This is a very long description of product #5.', 42.59, 0.0, '/images/test-product-5.png', false);
 
-INSERT INTO inventory (product_id, quantity)
-    VALUES (1, 5)
-    , (2, 8)
-    , (3, 12)
-    , (4, 73)
-    , (5, 2);
+INSERT INTO inventory (product_id, quantity, deleted)
+    VALUES (1, 5, false)
+    , (2, 8, false)
+    , (3, 12, false)
+    , (4, 73, false)
+    , (5, 2, false);
 
 INSERT INTO web_order (address_id, user_id)
     VALUES (1, 1)
