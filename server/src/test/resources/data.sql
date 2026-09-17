0ab9fc886b6321f3414ceee4a3451d4e0ec17bc5
maxim-polyakov <maxim7012@gmail.com>
2026-09-18 02:22:27 +0300

    Update data.sql

--- server/src/test/resources/data.sql
+++ server/src/test/resources/data.sql
@@ -2,21 +2,21 @@
 
 -- Passwords are in the format: Password<UserLetter>123. Unless specified otherwise.
 -- Encrypted using https://www.javainuse.com/onlineBcrypt
-INSERT INTO local_user (email, first_name, last_name, password, username, email_verified)
-    VALUES ('UserA@junit.com', 'UserA-FirstName', 'UserA-LastName', '$2a$10$hBn5gu6cGelJNiE6DDsaBOmZgyumCSzVwrOK/37FWgJ6aLIdZSSI2', 'UserA', true)
-    , ('UserB@junit.com', 'UserB-FirstName', 'UserB-LastName', '$2a$10$TlYbg57fqOy/1LJjispkjuSIvFJXbh3fy0J9fvHnCpuntZOITAjVG', 'UserB', false)
-    , ('UserC@junit.com', 'UserC-FirstName', 'UserC-LastName', '$2a$10$SYiYAIW80gDh39jwSaPyiuKGuhrLi7xTUjocL..NOx/1COWe5P03.', 'UserC', false);
+INSERT INTO local_user (email, first_name, last_name, password, username, email_verified, role)
+    VALUES ('UserA@junit.com', 'UserA-FirstName', 'UserA-LastName', '$2a$10$hBn5gu6cGelJNiE6DDsaBOmZgyumCSzVwrOK/37FWgJ6aLIdZSSI2', 'UserA', true, 'USER')
+    , ('UserB@junit.com', 'UserB-FirstName', 'UserB-LastName', '$2a$10$TlYbg57fqOy/1LJjispkjuSIvFJXbh3fy0J9fvHnCpuntZOITAjVG', 'UserB', false, 'USER')
+    , ('UserC@junit.com', 'UserC-FirstName', 'UserC-LastName', '$2a$10$SYiYAIW80gDh39jwSaPyiuKGuhrLi7xTUjocL..NOx/1COWe5P03.', 'UserC', false, 'USER');
 
-INSERT INTO address(address_line_1, city, country, user_id)
+INSERT INTO address(address_line, city, country, user_id)
     VALUES ('123 Tester Hill', 'Testerton', 'England', 1)
     , ('312 Spring Boot', 'Hibernate', 'England', 3);
 
-INSERT INTO product (name, short_description, long_description, price)
-    VALUES ('Product #1', 'Product one short description.', 'This is a very long description of product #1.', 5.50)
-    , ('Product #2', 'Product two short description.', 'This is a very long description of product #2.', 10.56)
-    , ('Product #3', 'Product three short description.', 'This is a very long description of product #3.', 2.74)
-    , ('Product #4', 'Product four short description.', 'This is a very long description of product #4.', 15.69)
-    , ('Product #5', 'Product five short description.', 'This is a very long description of product #5.', 42.59);
+INSERT INTO product (name, short_description, long_description, price, raiting, image)
+    VALUES ('Product #1', 'Product one short description.', 'This is a very long description of product #1.', 5.50, 0.0, '/images/test-product-1.png')
+    , ('Product #2', 'Product two short description.', 'This is a very long description of product #2.', 10.56, 0.0, '/images/test-product-2.png')
+    , ('Product #3', 'Product three short description.', 'This is a very long description of product #3.', 2.74, 0.0, '/images/test-product-3.png')
+    , ('Product #4', 'Product four short description.', 'This is a very long description of product #4.', 15.69, 0.0, '/images/test-product-4.png')
+    , ('Product #5', 'Product five short description.', 'This is a very long description of product #5.', 42.59, 0.0, '/images/test-product-5.png');
 
 INSERT INTO inventory (product_id, quantity)
     VALUES (1, 5)
